4b966f0d174fb2c0fa09b02005b6658fa45ace7b
Davide <daviderendina9@gmail.com>
2026-01-08 18:54:53 +0100

    Improved poiOnMap UI

--- lib/components/map_page/poi_detail_on_map.dart
+++ lib/components/map_page/poi_detail_on_map.dart
@@ -1,22 +1,26 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:myplaces/providers.dart';
 
+import '../../models/my_list.dart';
 import '../../models/poi.dart';
 
-class PoiDetailOnMap extends StatelessWidget {
+class PoiDetailOnMap extends ConsumerWidget {
   // TODO quando carico questo devo vedere prima se `e presente nel DB e comportarmi di conseguenza
   final Poi poi;
 
   const PoiDetailOnMap({super.key, required this.poi});
 
   @override
-  Widget build(BuildContext context) {
-    bool isSelected = false;
+  Widget build(BuildContext context, WidgetRef ref) {
+    List<MyList> allDefinedLists = ref.read(myListRepositoryProvider).getAll();
 
     // TODO spostare di qui quando verranno gestite per bene
     String listName = "Canada";
     IconData listIcon = Icons.landscape;
-    bool listSelected = true;
+
+    Color whiteColor = Colors.white.withAlpha(190);
 
     return Container(
       color: Colors.black,
@@ -72,108 +76,70 @@ class PoiDetailOnMap extends StatelessWidget {
 
           SizedBox(height: 24),
 
-          // TODO
-          // Row(
-          //   mainAxisAlignment: MainAxisAlignment.center,
-          //   spacing: 24,
-          //   children: DefaultListElement.values.map((element) {
-          //     return Container(
-          //       decoration: BoxDecoration(
-          //         borderRadius: BorderRadius.circular(30),
-          //         color: isSelected
-          //             ? element.listElement.color
-          //             : Colors.grey.shade800,
-          //       ),
-          //       child: IconButton(
-          //         onPressed: () {
-          //           print("X");
-          //         },
-          //         icon: Icon(
-          //           element.listElement.icon,
-          //           size: 32,
-          //           color: isSelected ? Colors.white : Colors.grey.shade700,
-          //         ),
-          //       ),
-          //     );
-          //   }).toList(),
-          // ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 24,
+            children: allDefinedLists.where((l) => l.isDefault).map((myList) {
+              bool selected = myList.name == "visited";
+              return Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: selected
+                          ? Colors.teal
+                          : Theme.of(context).colorScheme.surface,
+                    ),
+                    child: IconButton(
+                      onPressed: () {
+                        print("X");
+                      },
+                      icon: Icon(
+                        Icons.question_mark, //TODO myList.listElement.icon,
+                        size: 30,
+                        color: selected ? whiteColor : Colors.grey.shade800,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 4),
+
+                  Text(
+                    myList.displayName,
+                    style: TextStyle(
+                      color: selected ? whiteColor : Colors.grey.shade800,
+                    ),
+                  ),
+                ],
+              );
+            }).toList(),
+          ),
           SizedBox(height: 18),
 
           Wrap(
-            children: [
-              FilterChip(
+            spacing: 5,
+            children: allDefinedLists.where((l) => !l.isDefault).map((myList) {
+              bool selected = myList.name.length == 1 ? true : false;
+              return FilterChip(
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(20.0),
-                  side: BorderSide(color: Colors.white),
+                  side: BorderSide(color: Colors.transparent),
                 ),
-                selected: true,
+                selected: selected,
                 showCheckmark: false,
-                label: Text(listName),
-                avatar: Icon(listIcon),
-                onSelected: (value) {},
-                selectedColor: Colors.deepPurple,
-              ),
-              FilterChip(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                  side: BorderSide(color: Colors.white),
+                label: Text(
+                  myList.name,
+                  style: TextStyle(
+                    color: selected ? whiteColor : Colors.grey.shade800,
+                  ),
                 ),
-                selected: true,
-                showCheckmark: false,
-                label: Text(listName),
-                avatar: Icon(listIcon),
-                onSelected: (value) {},
-                selectedColor: Colors.blueAccent,
-              ),
-              FilterChip(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                  side: BorderSide(color: Colors.white),
+                avatar: Icon(
+                  Icons.question_mark,
+                  color: selected ? whiteColor : Colors.grey.shade800,
                 ),
-                selected: false,
-                showCheckmark: false,
-                label: Text(listName),
-                avatar: Icon(listIcon),
                 onSelected: (value) {},
-                selectedColor: Colors.deepPurple,
-              ),
-              FilterChip(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                  side: BorderSide(color: Colors.white),
-                ),
-                selected: false,
-                showCheckmark: false,
-                label: Text(listName),
-                avatar: Icon(listIcon),
-                onSelected: (value) {},
-                selectedColor: Colors.deepPurple,
-              ),
-              FilterChip(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                  side: BorderSide(color: Colors.white),
-                ),
-                selected: false,
-                showCheckmark: false,
-                label: Text(listName),
-                avatar: Icon(listIcon),
-                onSelected: (value) {},
-                selectedColor: Colors.deepPurple,
-              ),
-              FilterChip(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                  side: BorderSide(color: Colors.white),
-                ),
-                selected: false,
-                showCheckmark: false,
-                label: Text(listName),
-                avatar: Icon(listIcon),
-                onSelected: (value) {},
-                selectedColor: Colors.deepPurple,
-              ),
-            ],
+                selectedColor: Colors.teal.shade600,
+              );
+            }).toList(),
           ),
         ],
       ),
